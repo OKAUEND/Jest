be3bebd9fc75c7a4d524aff8ede1da1e98f2758d
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-09-04 10:51:04 +0900

    チェックボックスコンポーネントのテスト

---
+++ tests/unit/AtomCheckbox.spec.js
@@ -0,0 +1,44 @@
+import { mount } from '@vue/test-utils'
+import Checkbox from '@/components/AtomCheckbox.vue'
+
+describe('AtomCheckbox.vue',()=>{
+    it('Checkをすることができるか',()=>{
+        const wrapper = mount(Checkbox)
+        wrapper.setChecked(true)
+        expect(wrapper.element.checked).toBe(true)
+    })
+
+    it('propsを受け取れているか',()=>{
+        const ObjectData = {
+            test:{
+                ID:1,
+                Name:"TEST",
+                ItemLevel:1,
+                ImagePath:"icon/logo.png",
+                ProductionCount:1,
+            }
+        }
+        const wrapper = mount(Checkbox,{
+            propsData:ObjectData
+        })
+        expect(wrapper.props('test')).toEqual(ObjectData.test)
+    })
+
+    it('check時にemitでpropsのデータが返されてくるか',()=>{
+        const ObjectData = {
+            test:{
+                ID:1,
+                Name:"TEST",
+                ItemLevel:1,
+                ImagePath:"icon/logo.png",
+                ProductionCount:1,
+            }
+        }
+        const wrapper = mount(Checkbox,{
+            propsData:ObjectData
+        })
+        wrapper.setChecked(true)
+        wrapper.trigger('click')
+        expect(wrapper.emitted().change[0][0]).toEqual(ObjectData.test)
+    })
+})
