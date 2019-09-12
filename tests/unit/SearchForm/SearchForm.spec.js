3b11c7d6048c5974c298c4b9b672790a75433d32
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-09-13 07:21:37 +0900

    ADD - 参照コンポーネントを追加 $emitテスト用にAtomのボタンとInputTextを追加しておく

--- tests/unit/SearchForm/SearchForm.spec.js
+++ tests/unit/SearchForm/SearchForm.spec.js
@@ -1,5 +1,7 @@
-import { mount } from '@vue/test-utils'
-import SearchForm from '@/components/SearchForm/index.vue'
+import { mount, shallowMount } from '@vue/test-utils'
+import SearchForm from '@/components/Molecules/SearchForm/index.vue'
+import atomButton from '@/components/Atom/button/atomButton.vue'
+import atomInput from '@/components/Atom/Input/AtomInputText.vue'
 
 describe('SearchForm.vue',()=>{
     test('Dataに値を設定できるか',()=>{
