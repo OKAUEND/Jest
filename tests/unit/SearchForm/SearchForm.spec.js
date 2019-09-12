52316642ea731136c3546d8fa0f5194293e7211f
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-09-13 07:20:37 +0900

    ADD - テスト項目を作成

--- tests/unit/SearchForm/SearchForm.spec.js
+++ tests/unit/SearchForm/SearchForm.spec.js
@@ -2,5 +2,15 @@ import { mount } from '@vue/test-utils'
 import SearchForm from '@/components/SearchForm/index.vue'
 
 describe('SearchForm.vue',()=>{
+    test('Dataに値を設定できるか',()=>{
+    })
+
+    test('Buttonが存在するか',()=>{
+    })
+
+    test('inputが存在するか',()=>{
+    })
+    test('クリックしたらemitで親にイベントが通知されるか',()=>{
+    })
 
 })
