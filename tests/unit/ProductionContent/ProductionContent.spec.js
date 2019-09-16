894e5ff5d89b0d1b49c44494d125ec68e8c46524
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-09-16 13:16:32 +0900

    ADD - Atomをfindできるように参照を追加 / 既存参照設定のパス指定ミスを修正

--- tests/unit/ProductionContent/ProductionContent.spec.js
+++ tests/unit/ProductionContent/ProductionContent.spec.js
@@ -1,5 +1,8 @@
-import { mount , shallmount } from '@vue/test-utils'
-import ProductionCont from '@/component/Molecules/ProductionContent/index.vue'
+import { mount , shallowMount } from '@vue/test-utils'
+import ProductionCont from '@/components/Molecules/ProductionContent/index.vue'
+import atomButton from '@/components/Atom/button/atomButton.vue'
+import atomImage from '@/components/Atom/Image/AtomImage.vue'
+import ChildrenItem from '@/components/Molecules/ChildrenItem/index.vue'
 
 describe('ProductionContent.vue',()=>{
 
